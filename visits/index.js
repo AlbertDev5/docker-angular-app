052ddd3cbbc9de715bb194aa7bc21837a87938ea
AlbertDev <albert275djt@gmail.com>
2020-04-18 09:48:52 -0500

    docker compose, se comenta simulacion de caida de contenedor

--- visits/index.js
+++ visits/index.js
@@ -4,14 +4,15 @@ const process = require('process');
 
 const app = express();
 const client = redis.createClient({
-    host: 'redis-server',
+    // host: 'https:my.domain.com',
+    host: 'redis-server', // se puede referenciar el nombre del servicio indicado en docker compose
     port: 6379
 });
 
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
-    process.exit(0); // simular que se cae el contenedor
+    // process.exit(0); // simular que se cae el contenedor
     client.get('visits', (err, visits) => {
        res.send('Number of visits is ' + visits);
        client.set('visits', parseInt(visits) + 1);
